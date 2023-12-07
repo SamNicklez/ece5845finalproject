9b5d8e9935b3bc2b32c04ad2fe2236d0fc913fde
Colin Hehn <chehn@uiowa.edu>
2023-12-07 13:48:14 -0600

    Got demo ready for query drafting

--- demo.py
+++ demo.py
@@ -2,14 +2,41 @@ from neo4j import GraphDatabase as gd
 import neo4j.exceptions
 import psycopg2 as pg
 
-# Demo Neo4J server connection
+# QUERY DEMO RUN
+
+'''
+1. [PostgreSQL] Return the top 10 jobs based on a user's inputted preferences, some of our options being salary, location, benefits,
+and reviews (still unsure which will be available to the user at the moment). The user will be able to configure the importance
+of each of the preferences, such that the rank of jobs returned will be ordered using the 'weights' of each preference.
+We also plan to allow the user to filter the results by a specific preference (i.e only jobs in the Netherlands).
+'''
+conn = None
+try:
+    conn = pg.connect(
+        host="s-l112.engr.uiowa.edu",
+        port=5432,
+        database="mdb_student18",
+        # Probably should make this config instead, but it's fine for now
+        user='mdb_student18',
+        password='finalproject2023')
+    cur = conn.cursor()
+    cur.execute('')
+    top_10_jobs = cur.fetchall()
+    for job in top_10_jobs:
+        print(job)
+except (Exception, pg.DatabaseError) as error:
+    print(error)
+
+'''
+2. [Neo4J] We will cluster companies around countries/cities, and allow the user to query a city or company and return the other
+companies in the cluster. Hoping to add a feature that allows the user to search for certain keywords in employee reviews as well.
+'''
 driver = gd.driver("neo4j://localhost:7687", auth=("neo4j", "hello"))
 try:
     driver.verify_connectivity()
 except neo4j.exceptions.ServiceUnavailable:
     print('Connection to Neo4j database failed. Please try again.')
     exit()
-
 with driver.session() as sesh:
     def keyword():
         keyword = input("Please enter a keyword (or a few, separated by spaces): ")
@@ -19,40 +46,7 @@ with driver.session() as sesh:
                  "RETURN m.title AS movieTitle, g.name AS genre, avgRating AS averageRating, seenMovie AS seenMovie, userRating AS userRating;"
                 )
 
-        result = sesh.run(query, input_user=user_id, keywordsList=keyword.split()).data()
-        print('Listing movie titles containing keyword(s): ' + keyword + '.')
+        result = sesh.run(query, input_user='1', keywordsList=keyword.split()).data()
         for i in range(len(result)):
-            print((str)(i+1) + ". " + result[i]['movieTitle'] + " (" + result[i]['genre'] + ", avg rating: " + (str)(result[i]['averageRating']) + ", seen: " + (str)(result[i]['seenMovie']) + ", your rating: " + (str)(result[i]['userRating']) + ")")
+            print((str)(i+1) + ". " + result[i]['field'])
         return
-
-
-# Demo PostgreSQL server connection
-conn = None
-try:
-    file = open("credentials.txt")
-    file_content = file.readlines()
-
-    # connect to the PostgreSQL server
-    print('Connecting to the PostgreSQL database...')
-    conn = pg.connect(
-        host="s-l112.engr.uiowa.edu",
-        port=5432,
-        database="mdb_student37",
-        user=file_content[0],
-        password=file_content[1])
-		
-    # create a cursor
-    cur = conn.cursor()
-    
-    # execute a statement
-    cur.execute('SELECT version();')
-
-    # display the PostgreSQL database server version
-    db_version = cur.fetchone()
-    print(db_version)
-    cur.close()
-except (Exception, pg.DatabaseError) as error:
-    print(error)
-
-
-# HOW OUR QUERIES LOOK IN PYTHON
