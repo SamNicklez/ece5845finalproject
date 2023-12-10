9dc6bbc1042f57c43fe6dfaeea9498d050ae287f
SamNicklez <snicklaus@uiowa.edu>
2023-12-10 14:44:49 -0600

    pushed fetch command

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -10,18 +10,22 @@
         <v-window-item value="one" class="query-one">
           <h1>Top 10 jobs based on a your inputted preferences</h1>
           <h2>Please select your preferred country</h2>
-          <v-autocomplete v-model="selectedCountry" label="Input Country" :items="countries" @blur="submitCountry"></v-autocomplete>
-          <h2 v-if="this.selectedCountry" >Please select your preferred city</h2>
-          <v-autocomplete v-model="selectedCity" v-if="this.selectedCountry" label="Input City" :items="cities" @blur="submitCity"></v-autocomplete>
-          <h2 v-if="this.selectedCity" >Please select your preferred sector</h2>
-          <v-autocomplete v-model="selectedSector" v-if="this.selectedCity" label="Input City" :items="sectors"></v-autocomplete>
+          <v-autocomplete v-model="selectedCountry" label="Input Country" :items="countries"
+            @blur="submitCountry"></v-autocomplete>
+          <h2 v-if="this.selectedCountry">Please select your preferred city</h2>
+          <v-autocomplete v-model="selectedCity" v-if="this.selectedCountry" label="Input City" :items="cities"
+            @blur="submitCity"></v-autocomplete>
+          <h2 v-if="this.selectedCity">Please select your preferred sector</h2>
+          <v-autocomplete v-model="selectedSector" v-if="this.selectedCity" label="Input City"
+            :items="sectors"></v-autocomplete>
           <h2 v-if="this.selectedSector">Please rank these company traits (top being highest priority)</h2>
           <draggable v-if="this.selectedSector" class="draggable-container" :list="list" @change="log">
-            <div class="draggable-text-container" v-for="element,index in list" :key="element.name">
-              <p>{{ index + 1  }}. {{ element.name }}</p>
+            <div class="draggable-text-container" v-for="element, index in list" :key="element.name">
+              <p>{{ index + 1 }}. {{ element.name }}</p>
             </div>
           </draggable>
-          <v-btn v-if="this.selectedSector" @click="calculatePostgreSQLQuery" style="margin-top: 2.5vh; margin-bottom: 5vh;" color="deep-purple-darken-2">Submit</v-btn> 
+          <v-btn v-if="this.selectedSector" @click="calculatePostgreSQLQuery"
+            style="margin-top: 2.5vh; margin-bottom: 5vh;" color="deep-purple-darken-2">Submit</v-btn>
         </v-window-item>
         <v-window-item value="two">
           Two
@@ -70,11 +74,11 @@ export default defineComponent({
       console.log(event)
       console.log(this.list)
     },
-    submitCountry(){
+    submitCountry() {
       console.log(this.selectedCountry)
       //Grab list of cities from backend and set to dropdown
     },
-    submitCity(){
+    submitCity() {
       console.log(this.selectedCity)
       //Grab list of cities from backend and set to dropdown
     },
@@ -86,7 +90,7 @@ export default defineComponent({
       console.log(preferences)
       //Grab list of cities from backend and set to dropdown
     },
-    reset(){
+    reset() {
       this.selectedCountry = null
       this.selectedCity = null
       this.selectedSector = null
@@ -95,6 +99,24 @@ export default defineComponent({
   },
   async created() {
     //Grab list of countries from backend and set to dropdown
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    };
+
+    fetch("http://127.0.0.1:5000/countries/distinct", requestOptions)
+      .then(response => response.json()) // Assuming the response is in JSON format
+      .then(result => {
+        this.countries = result.map(country =>
+          country.charAt(0).toUpperCase() + country.slice(1).toLowerCase()
+        );
+        console.log(this.countries);
+      })
+      .catch(error => console.log('error', error));
+
   },
 })
 </script>
@@ -132,10 +154,12 @@ h1 {
   font-size: 2em;
   padding: 1em;
 }
+
 h2 {
   font-size: 1.5em;
   margin-top: 5vh;
 }
+
 .query-one {
   padding: 1em;
 }
