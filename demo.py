01e800394018174ddc8e146234ecabd07a97de7d
Colin Hehn <chehn@uiowa.edu>
2023-12-10 18:09:49 -0600

    "finished" get_similar_jobs()

--- demo.py
+++ demo.py
@@ -2,7 +2,6 @@ from neo4j import GraphDatabase as gd
 import neo4j.exceptions
 import psycopg2 as pg
 from sklearn.cluster import KMeans
-import numpy as np
 
 postgres_conn = None
 
@@ -36,64 +35,57 @@ def query_1():
     neo_similarity_query = """
         WITH $job_review_preferences AS job_review_preferences
         MATCH (r:Review)
-        WITH r, job_review_preferences
+        WITH r, job_review_preferences,
             [
-                r.opportunities_ranking,
-                r.comp_benefits_ranking,
-                r.culture_values_ranking,
-                r.senior_management_ranking,
-                r.worklife_balance_ranking,
-                r.ceo_approval_ranking,
-                r.company_outlook_ranking
-            ] AS job_review_rankings
-        WITH r, gds.similarity.cosine(job_review_preferences, job_review_rankings) AS similarity
+                r.opportunities_rank,
+                r.comp_benefits_rank,
+                r.culture_values_rank,
+                r.senior_management_rank,
+                r.worklife_balance_rank,
+                r.ceo_approval_rank,
+                r.company_outlook_rank
+            ] AS job_review_ranks
+        WITH r, gds.similarity.cosine(job_review_preferences, job_review_ranks) AS similarity
         RETURN r.review_id AS reviewId, similarity AS cosineSimilarity
         ORDER BY similarity DESC
         LIMIT 10
         """
-    
-    postgres_job_info_query = """
-        SELECT j.job_title, c.company_name
-        """
 
     with driver.session() as session:
-        cluster_reviews()
-
         # Grab relevant jobs with user scores
         demo_job_review_preferences = [3, 5, 7, 2, 1, 6, 4]
         result = session.run(neo_similarity_query, job_review_preferences=demo_job_review_preferences)
         jobs = [job for job in result]
 
-        # Grab relevant job info from PostgreSQL
+    # try:
+    #         # Configure PostgreSQL connection
+    #     postgres_conn = pg.connect(
+    #         host="s-l112.engr.uiowa.edu",
+    #         port=5432,
+    #         database="mdb_student18",
+    #         user='mdb_student18',
+    #         password='finalproject2023')
+    #     cur = postgres_conn.cursor()
 
-    try:
-            # Configure PostgreSQL connection
-        postgres_conn = pg.connect(
-            host="s-l112.engr.uiowa.edu",
-            port=5432,
-            database="mdb_student18",
-            user='mdb_student18',
-            password='finalproject2023')
-        cur = postgres_conn.cursor()
-
-    except (Exception, pg.DatabaseError) as error:
-        print(error)
-    finally:
-        if postgres_conn is not None:
-            postgres_conn.close()
+    # except (Exception, pg.DatabaseError) as error:
+    #     print(error)
+    # finally:
+    #     if postgres_conn is not None:
+    #         postgres_conn.close()
     return
 
+'''
+2. [Neo4J] We will cluster companies around countries/cities, and allow the user to query a city or company and return the other
+companies in the cluster. Hoping to add a feature that allows the user to search for certain keywords in employee reviews as well.
+'''
 def cluster_reviews(song_features, num_clusters=3):
     # Cluster the songs
     kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(song_features)
     centroids = kmeans.cluster_centers_
     return centroids
 
-'''
-2. [Neo4J] We will cluster companies around countries/cities, and allow the user to query a city or company and return the other
-companies in the cluster. Hoping to add a feature that allows the user to search for certain keywords in employee reviews as well.
-'''
 def query_2():
     return
+
 
 query_1()
--- flask_server/neo4j_db.py
+++ flask_server/neo4j_db.py
@@ -1,12 +1,60 @@
-from neo4j import GraphDatabase
-
+from neo4j import GraphDatabase as gd
+import neo4j.exceptions
 
 def get_neo4j_db(uri, user, password):
-    return GraphDatabase.driver(
+    return gd.driver(
         uri=uri,
         auth=(user, password)
     )
 
+def get_similar_jobs(driver, country, city, sector, ranks):
+    '''
+    1. User inputs a country, city, sector, and then their order of importance 1-7 for each of the following:
+        - opportunities_ranking
+        - comp_benefits_ranking
+        - culture_values_ranking
+        - senior_management_ranking
+        - worklife_balance_ranking
+        - ceo_approval_ranking
+        - company_outlook_ranking
 
-def get_similar_jobs(tx, country, city, sector, ranks):
-    return "TODO"
+    2. Values of the user's preferences comes from the frontend, with which we construct a pseudo-review node.
+
+    3. We then use the cosine similarity algorithm to find the most 10 most similar reviews to the pseudo-review.
+
+    4. We then return the top 10 jobs associated with those reviews.
+    '''
+
+    neo_similarity_query = """
+    WITH $job_review_preferences AS job_review_preferences
+    MATCH (r:Review)-[:REVIEW_FOR]->(j:Job)
+    WHERE j.country = $country AND j.city = $city AND j.sector = $sector
+    WITH r, job_review_preferences,
+        [
+            r.opportunities_rank,
+            r.comp_benefits_rank,
+            r.culture_values_rank,
+            r.senior_management_rank,
+            r.worklife_balance_rank,
+            r.ceo_approval_rank,
+            r.company_outlook_rank
+        ] AS job_review_ranks
+    WITH r, gds.similarity.cosine(job_review_preferences, job_review_ranks) AS similarity
+    RETURN r.review_id AS reviewId, similarity AS cosineSimilarity
+    ORDER BY similarity DESC
+    LIMIT 10
+    """
+
+    # switch this to use the driver parameter in the function
+    with driver.session() as session:
+        # Grab relevant jobs with user scores
+        result = session.run(neo_similarity_query, job_review_preferences=ranks, country=country, city=city, sector=sector)
+        reviews = [review for review in result]
+    review_ids = []
+    for review in reviews:
+        review_ids.append(review["reviewId"])
+        # print(review["cosineSimilarity"])
+
+    return review_ids
+
+# print(get_similar_jobs(None, "United States", "New York, NY", "Information Technology", [3, 5, 7, 2, 1, 6, 4]))
